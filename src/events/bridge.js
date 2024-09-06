08c97d776f1facc4817ffc7d934234c75c0e75e4
Raymundo Paz <raymundo.paz@autozone.com>
2024-09-06 11:33:54 -0600

    Clean slate

--- src/events/bridge.js
+++
@@ -1,9 +0,0 @@
-const { ipcMain } = require('electron');
-
-const windowEventsManager = require('./windowEventsManager');
-const { fileManager } = require('./fileEventsManager');
-const { dataManager } = require('./dataEventsManager');
-
-ipcMain.on('window-events', windowEventsManager);
-ipcMain.on('data-events', dataManager.manageDataEvents);
-ipcMain.on('file-events', fileManager.manageFileEvents);
